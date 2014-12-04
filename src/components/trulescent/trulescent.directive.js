11a76c9da96bbbe4e809af09fb6957bc157d01e1
FranzP <francois.poizat@gmail.com>
2014-12-04 15:20:52 +0100

    added watch, event and pre and post callbacks

--- src/components/trulescent/trulescent.directive.js
+++ src/components/trulescent/trulescent.directive.js
@@ -1,24 +1,68 @@
 'use strict';
 
 angular.module('trulescent')
-  .directive('trulescent', function ($timeout ) {
+  .directive('trulescent', function ( $timeout, tlscTools ) {
     return {
       restrict : 'EA',
       scope:{ start: '=' , steps: '=' },
       templateUrl : 'components/trulescent/trulescent.html',
       controller :'trulescentCtrl',
       link: function ( $scope, $element, $attrs ) {
-        var stepIndex = 0;
+        var stepIndex = 0,
+          animationDurationMilli = 200;
 
-        function goToStep( step ) {
-          $scope.content = $scope.steps[step].content;
+        function setUpTransition( step ) {
+          if ( step.transition.event ) {
 
-          //TODO(franz): better than that
-          $timeout(function() {
-            $scope.placement = $scope.steps[step].placement;
-          },250);
+            var targetEl = angular.element( document.querySelector( step.transition.selector ) );
 
+            targetEl.on( step.transition.event,
+              function ( $event ) {
+
+                targetEl.off( 'click' );
+                goToStep( ++stepIndex, stepIndex - 1 );
+
+                step.postFn();
+
+                $scope.$apply();
+              }
+            );
+
+          } else if (step.transition.watch) {
+
+            var targetEl = angular.element( document.querySelector( step.transition.selector ) );
+            if (targetEl.length) {
+              var targetScope = targetEl.data('$scope');
+              var transitionWatch = targetScope.$watch( step.transition.watch,
+                function ( newValue, oldValue ) {
+                  if ( angular.isDefined( newValue ) &&
+                    ( step.transition.testValue ? newValue === step.transition.testValue : true ) ) {
+
+                    transitionWatch();
+                    goToStep( ++stepIndex, stepIndex - 1 );
+
+                    targetScope[step.transition.watch] = step.transition.afterValue || targetScope[step.transition.watch];
+
+                    step.postFn();
+                  }
+                }
+              );
+            }
+          }
+        }
+
+        function goToStep( step, prevStep ) {
+          $scope.step = $scope.steps[step];
           $scope.$broadcast( 'tlsc.backdrop.move', $scope.steps[step].location.selector );
+
+          if(prevStep >= 0) {
+              angular.element(document.querySelector($scope.steps[prevStep].location.selector)).removeClass('tlsc-element');
+          }
+          angular.element(document.querySelector($scope.steps[step].location.selector)).addClass('tlsc-element');
+
+          $scope.steps[step].preFn && $scope.steps[step].preFn();
+
+          setUpTransition($scope.steps[step]);
         };
 
         $scope.$watch( 'start', function ( newValue, oldValue ) {
@@ -28,8 +72,18 @@ angular.module('trulescent')
         });
 
         $scope.$on('tlsc.next',function($event) {
-          goToStep(++stepIndex);
-        })
+          goToStep(++stepIndex, stepIndex - 1);
+        });
+
+        $scope.$on('tlsc.prev',function($event) {
+          goToStep(--stepIndex, stepIndex + 1);
+        });
+
+        $scope.$on('tlsc.end',function($event) {
+          stepIndex = 0;
+          angular.element(document.querySelector('.tlsc-element')).removeClass('tlsc-element');
+          $scope.start = false;
+        });
       }
     }
   });
