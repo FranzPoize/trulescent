96987866dff33cd15d6a5f79293ed53ea72f94a7
FranzP <francois.poizat@gmail.com>
2015-03-18 16:26:16 +0100

    new build version

--- dist/trulescent.js
+++ dist/trulescent.js
@@ -112,8 +112,8 @@ angular.module('trulescent')
           if ( angular.isDefined( newValue ) ) {
 
             if ( newValue.tooltip.template ) {
-              $scope.content = newValue.tooltip.content;
-              $element.empty().append($compile(newValue.tooltip.template)($scope));
+              var templateScope = angular.extend($scope.new(),newValue.tooltip.templateScope);
+              $element.empty().append($compile(newValue.tooltip.template)(templateScope));
             } else {
               $element.empty().append($compile($templateCache.get("components/tooltip/tooltip.html"))($scope));
             }
